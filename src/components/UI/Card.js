640b1ac46ef1c5123a805438bf278cecfbe19c27
John Cross <john.william.cross@gmail.com>
2021-06-05 17:18:16 -0700

    testing

--- src/components/UI/Card.js
+++ src/components/UI/Card.js
@@ -1,5 +1,7 @@
 import React from "react";
 import "./Card.css";
+//now is this wrking?
+
 const Card = (props) => {
   const classes = "card " + props.className;
   return <div className={classes}>{props.children}</div>;
