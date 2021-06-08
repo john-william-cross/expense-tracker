309a0da768c1d86fb5c98d4612908df2ba315329
John Cross <john.william.cross@gmail.com>
2021-06-08 15:47:04 -0700

    begin video 53 working with multiple states

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 
 import "./ExpenseForm.css";
-
+//
 const ExpenseForm = () => {
   const [enteredTitle, setEnteredTitle] = useState("");
   const [enteredAmount, setEnteredAmount] = useState("");
